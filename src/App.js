aafc0aa190e70b73e7ae9707c684892f61896882
Fig Yogurt <itisaftab@gmail.com>
2020-12-08 18:35:34 -0500

    feat(chatfeed): successfully did get request on initial data and pass down props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./App.css";
+
 import Header from "./Components/Header";
 import ChatFeed from "./Components/ChatFeed";
 import Footer from "./Components/Footer";
 
 function App() {
+  const [chatData, setChatData] = useState([]);
+
+  const url = "https://question-toy-server.herokuapp.com/api/questions";
+  useEffect(() => {
+    const firstFetch = async () => {
+      try {
+        const initialFetch = await axios.get(url);
+        setChatData(initialFetch.data.questions);
+      } catch (error) {
+        window.alert("Connection to API Failed");
+      }
+    };
+    firstFetch();
+  }, []);
+
   return (
     <div id="container">
       <Header />
-      <ChatFeed />
+      <ChatFeed chatData={chatData} />
       <Footer />
     </div>
   );
 }
 
 export default App;
+
+// const socket = new WebSocket(
+//   "wss://question-toy-server.herokuapp.com/api/questions"
+// );
+// socket.onopen = () => {
+//   console.log("Connected to server!");
+// };
+// socket.onmessage = (event) => {
+//   const message = event.data;
+//   console.log(message);
+// };
+// socket.onclose = () => {
+//   console.log("Disconnected from server!");
+// };
--- src/Components/ChatFeed.js
+++ src/Components/ChatFeed.js
@@ -1,49 +1,10 @@
 import React from "react";
 
-export default function ChatFeed() {
+export default function ChatFeed(props) {
+  const { chatData } = props;
+  console.log(chatData);
   return (
     <div className="chat-logs">
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
-      <div className="one-chat-log">hi</div>
       <div className="one-chat-log">hi</div>
     </div>
   );
