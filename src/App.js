d664472345dfcb70cd156707c4d4dbb0f0398ed6
Fig Yogurt <itisaftab@gmail.com>
2020-12-07 19:11:01 -0500

    feat(app): connected ChatFeed and ChatForm components to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from "./logo.svg";
 import "./App.css";
 import ChatHeader from "./Components/ChatHeader";
+import ChatFeed from "./Components/ChatFeed";
+import ChatForm from "./Components/ChatForm";
 
 function App() {
   return (
     <div>
       <ChatHeader />
+      <ChatFeed />
+      <ChatForm />
     </div>
   );
 }
