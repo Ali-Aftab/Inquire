8206039b5dbccccbdad15be4c3335a57a2c181cc
Fig Yogurt <itisaftab@gmail.com>
2020-12-07 19:08:53 -0500

    feat(app): removed original create-react-app html and place basic header, feed, and form

---
+++ src/Components/ChatFeed.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ChatFeed() {
+  return (
+    <div>
+      BareBlanket
+      <h1>Response</h1>
+    </div>
+  );
+}
---
+++ src/Components/ChatHeader.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ChatHeader() {
+  return (
+    <div>
+      <header>Inquire</header>
+    </div>
+  );
+}
