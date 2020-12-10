9800d56118fba93a183a00a3360a81ee7c42e195
Fig Yogurt <itisaftab@gmail.com>
2020-12-10 00:07:57 -0500

    style(chatfeed): added color and stylized the feed

--- src/Components/OneChatMessage.js
+++ src/Components/OneChatMessage.js
@@ -5,9 +5,13 @@ export default function OneChatMessage(props) {
   const { ind } = props;
 
   return (
-    <div className="one-chat-log" key={ind}>
-      <div key={`content${ind}`}>{content}</div>
-      <div key={`name${ind}`}>{`by ${firstName} ${lastName}`}</div>
+    <div className="one-chat-container" key={ind}>
+      <div className="one-chat-log" key={`content${ind}`}>
+        {content}
+      </div>
+      <div className="one-chat-user" key={`name${ind}`}>
+        {firstName} {lastName}
+      </div>
     </div>
   );
 }
